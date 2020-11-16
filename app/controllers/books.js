75583f2a795a3de6d9b5f0d1f228b4a91456ec98
_Rabbit_ <martynov-danil@list.ru>
2020-11-16 23:34:16 +0500

    Search Book simple

--- app/controllers/books.js
+++ app/controllers/books.js
@@ -1,9 +1,14 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 
 
 export default class BooksController extends Controller {
+  queryParams = ['search'];
+
+  @tracked search = '';
+  @tracked isLoading;
   @service dataService;
   @action
   async deleteBook(id) {
