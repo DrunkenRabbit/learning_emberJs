461e846b886aa08f7a9cc8371d63954e209185ad
_Rabbit_ <martynov-danil@list.ru>
2020-11-29 23:30:19 +0500

    update

--- app/router.js
+++ app/router.js
@@ -7,13 +7,13 @@ export default class Router extends EmberRouter {
 }
 
 Router.map(function() {
-  this.route('book', {path: "books"});
-  this.route('speaker', {path: "speakers"});
+  this.route('book', {path: "/books"});
+  this.route('speaker', {path: "/speakers"});
   this.route('edit-book', {path: "edit-book/:id"});
   this.route('edit-speaker', {path: "edit-speaker/:id"});
   this.route('create-book');
   this.route('create-speaker');
   this.route('not-found', { path: "*path"});
   this.route('error', { path: "/:error"});
-  this.route('meeting', {path: "meetings"});
+  this.route('meeting', {path: "/meetings"});
 });
