391b6ec846913e79da9f461e559dc71b18e473e4
_Rabbit_ <martynov-danil@list.ru>
2020-11-30 00:20:56 +0500

    update create-speaker.hbs

--- app/controllers/create-speaker.js
+++ app/controllers/create-speaker.js
@@ -1,28 +1,20 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
-import { inject as service } from '@ember/service';
 
-import { tracked } from '@glimmer/tracking';
 
 export default class CreateSpeakerController extends Controller {
-  @service dataService;
-
-  @tracked firstName;
-  @tracked lastName;
-  @tracked patronymic;
 
   @action
-  async saveSpeaker(e) {
-    e.preventDefault();
-
-    await this.dataService.createSpeaker(this.model);
-
-    this.transitionToRoute('speakers');
+  async saveSpeaker(speaker) {
+    try {
+      speaker.preventDefault();
+      let newSpeaker = this.store.createRecord('speaker', this.model);
+      await newSpeaker.save();
+      this.transitionToRoute('speaker');
+    }
+    catch(e) {
+      this.send('error', e);
+    }
   }
 
-  @action
-  changeName(firstName, lastName) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-  }
 }
